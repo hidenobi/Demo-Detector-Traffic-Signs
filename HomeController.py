6e3d9289b378b396e0cd509b4db655ae24c7e74b
TuanVH <dungtuan095@gmail.com>
2023-10-23 22:38:58 +0700

    load successful assign_model

--- HomeController.py
+++ HomeController.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import cv2
 app = Flask(__name__)
 
 # Dữ liệu người dùng giả định
@@ -20,14 +21,29 @@ def get_users():
 def receiveData():
     data = request.get_json()
     print(data)
-    result = processData(data)
+    result = int(processData(data))
     return jsonify(result)
     
-
+IMG_HEIGHT = 30
+IMG_WIDTH = 30
+channels = 3
+model_path = 'assign_model.h5'
+image_path = 'test.png'
 def processData(data):
-    model = keras.models.load_model('assign_model.h5')
-    image = np.array(Image.open("test.png"))
-    resized_image = np.reshape(image, (-1, 30, 30, 3))
-    predictions = model.predict(resized_image)
-    print(predictions)
-    return predictions
+    dataList = []
+    model = keras.models.load_model(model_path)
+    image = cv2.imread(image_path)
+    image_fromarray = Image.fromarray(image, 'RGB')
+    resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+    dataList.append(np.array(resize_image))
+    X_test = np.array(dataList)
+    X_test = X_test/255
+    pred = model.predict(X_test)
+    print('-------------')
+    array = pred[0]
+    print(len(array))
+    print(type(array))
+    max_index = np.argmax(array)
+    print(max_index)
+    print('-------------')
+    return max_index
